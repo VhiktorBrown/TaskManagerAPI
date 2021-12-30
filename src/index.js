82abccad23625be748c49bc5e58dbbc0c5c99ffc
VhiktorBrown <bukkychukwujekwu@gmail.com>
2021-12-30 21:40:40 +0100

    Ready for deployment. API done

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const taskRouter = require('./routes/task')
 
 const app = express()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 
 app.use(express.json())
 app.use(userRouter)
